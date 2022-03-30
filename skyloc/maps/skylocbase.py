8f28057d0d0e72eb76c0407b6fd583f9704cd7a9
Israel Martinez <imc@umd.edu>
2022-03-30 17:44:38 -0400

    Annular roi

--- skyloc/maps/skylocbase.py
+++ skyloc/maps/skylocbase.py
@@ -7,6 +7,8 @@ from astropy.coordinates import UnitSphericalRepresentation, SkyCoord
 import astropy.units as u
 from astropy.units import cds
 
+from mocpy import MOC
+
 class SkyLocBase(HealpixBase):
     """
     Initialize HealpixMap with an attached coordinate frame.
@@ -73,16 +75,16 @@ class SkyLocBase(HealpixBase):
             nside (int): NSIDE inside the disc.
         """
 
-        source_ang = center.represent_as(UnitSphericalRepresentation)
+        center_ang = center.represent_as(UnitSphericalRepresentation)
         
         # Empty map
         mEq = HealpixBase(nside = nside, scheme = 'ring')
         
-        source_vec = hp.ang2vec(source_ang.lon.deg,
-                                source_ang.lat.deg,
+        center_vec = hp.ang2vec(center_ang.lon.deg,
+                                center_ang.lat.deg,
                                 lonlat = True)
         
-        disc_pix = mEq.query_disc(source_vec, radius.to_value(u.rad))
+        disc_pix = mEq.query_disc(center_vec, radius.to_value(u.rad))
 
         # MOC map
         m = HealpixBase.moc_from_pixels(mEq.nside, disc_pix)
@@ -96,21 +98,56 @@ class SkyLocBase(HealpixBase):
                     center,
                     radius,
                     width,
-                    nside):
-        
+                    nside):        
         """
-        Create a probability for an annulus error.
-
-        The triangulation of a signal by two detector typically results 
-        in this pattern.
-        
-        The followin distribution is distribution is assumed.
+        Prepare a mesh with high resolution inside an annulus.
 
         Args:
-            skycoord (SkyCoord): Best estimate for the source location
-            radius (Angle or Quantity): Error radius 
-            cont (float): Fraction containment that the error radius 
-                corresponds to. Default is equivalent to radius = sigma
-            nside (int): Override the nside estimated based on the error radius.
+            center (SkyCoord): Center of the annulus
+            radius (Angle or Quantity): Angular distance from the center to the middle 
+                of the ring.
+            width (Angle or Quantity): Width of the ring --i.e. along the 
+                radial direction.
+            nside (int): NSIDE inside the annulus
         """
+
+        # Using mocpy due to this issue with healpy
+        # https://github.com/healpy/healpy/issues/641
+
+        center = center.represent_as(UnitSphericalRepresentation)
+
+        order = hp.nside2order(nside)
+        
+        # Outer disc
+        moc_o = MOC.from_cone(lon = center.lon,
+                              lat = center.lat,
+                              radius = radius + width/2,
+                              max_depth = order)
+
+        # Inner disc
+        moc_i = MOC.from_cone(lon = center.lon,
+                              lat = center.lat,
+                              radius = radius - width/2,
+                              max_depth = order)
+
+        # Annulus
+        moc = moc_o.difference(moc_i)
+
+        # Convert the pixel ranges into a pixel list
+        # There doesn't seem to be an access method for "._interval_set". Weird.
+        # Not ideal, I hope this doesn't break in future versions
+        # See https://github.com/cds-astro/mocpy/issues/72
+        
+        hires_pix = []
+
+        for i,f in np.transpose(hp.uniq2range(nside, moc._uniq_format())):
+            hires_pix += list(np.arange(i,f, dtype = int))
+
+        # Add the complement pixels in order to have a well defined mesh
+        uniq = np.append(hp.nest2uniq(nside, hires_pix), 
+                         moc.complement()._uniq_format().astype(int))
+
+        # Create the empty map
+        return cls(uniq = uniq)
+
 
